0f2f95ca004a76568fd7adbb0f0bec9a5fdaf7f4
ych4101861 <57085347+ych4101861@users.noreply.github.com>
2019-10-30 20:54:51 +0800

    加入專案檔案。

---
+++ 01JoinFun/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace _01JoinFun.Controllers
+{
+    public class LoginController : Controller
+    {
+        SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["JoinFunConnectionString"].ConnectionString);
+
+        // GET: Login
+        //登入
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Account, string Password)
+        {
+            string sql = "select * from tStudent where Account=@Account  and Password=@Password";
+
+            SqlCommand cmd = new SqlCommand(sql, Conn);
+            cmd.Parameters.AddWithValue("@Account", Account);
+            cmd.Parameters.AddWithValue("@Password", Password);
+            SqlDataReader rd;
+
+            Conn.Open();
+            rd = cmd.ExecuteReader();
+
+            if (rd.Read())
+            {
+                Session["Account"] = rd["Account"].ToString();
+                Session["Password"] = rd["Password"].ToString();
+
+                Conn.Close();
+                return RedirectToAction("Home");
+            }
+
+
+            Conn.Close();
+            ViewBag.LoginErr = "帳號或密碼有誤!!";
+            return View();
+
+        }
+        //成功登入首頁跳轉頁面
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        //成功登入首頁跳轉頁面
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //登出
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
